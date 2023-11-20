e64bdcfea27c8b6e2a291759a91550860e5288ad
jfaduna <flomeraduna@gmail.com>
2023-11-20 18:44:37 +1100

    modify: add basic styles for pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const methoOverride = require('method-override')
 const Campground = require('./models/campground')
 
@@ -14,11 +15,16 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methoOverride('_method'))
+
+app.get('/', (req, res) => {
+    res.send('HEY!')
+})
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
