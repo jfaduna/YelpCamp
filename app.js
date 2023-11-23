467239d15e46db35774bf87dce4a2a9851ef0b8a
jfaduna <flomeraduna@gmail.com>
2023-11-23 23:29:47 +1100

    modify: serve static assets

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methoOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/campgrounds', campgrounds)
