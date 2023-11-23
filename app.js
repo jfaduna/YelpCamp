2bd9d6cd9e3cc78ec424d69378bd35140809560e
jfaduna <flomeraduna@gmail.com>
2023-11-24 00:31:23 +1100

    add: add flash for each CRUD

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose');
+const session = require('express-session')
+const flash = require('connect-flash')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const methoOverride = require('method-override')
@@ -26,6 +28,24 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methoOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
