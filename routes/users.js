bb383ab64f82e8e5ed5149a3404ce82c7213fac0
jfaduna <flomeraduna@gmail.com>
2023-12-06 14:54:22 +0800

    add: login, register, and logout button

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,14 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Successfully logged out')
+        res.redirect('/campgrounds')
+    });
+})
+
 module.exports = router;
